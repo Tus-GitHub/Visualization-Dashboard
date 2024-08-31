b764ecec05a2848afe2db780986cb2690e491813
Tus-GitHub <tushardhull22@gmail.com>
2024-08-31 21:16:23 +0530

    creating dropdown for Charts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
 import About from './pages/About'
+import Charts from './pages/Charts'
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -16,6 +17,7 @@ export default function App() {
     <Routes>
       <Route path="/" element ={<Home />} />
       <Route path="/about" element ={<About />} />
+      <Route path="/charts" element = {<Charts/>}/>
     </Routes>
     </div>
     </BrowserRouter>
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 export default function Sidebar({isOpen, toggleMenu}){
+
+  const [isDropDown, setIsDropDown] = useState({});
+
+  const toggleDropDown = () =>{
+    setIsDropDown(!isDropDown);
+  };
     return (
         <div className={`fixed top-0 left-0 h-full bg-gray-800 shadow-xl transition-transform duration-300 ease-in-out transform  text-white ${
             isOpen ? 'translate-x-0' : '-translate-x-full'
@@ -26,8 +32,47 @@ export default function Sidebar({isOpen, toggleMenu}){
           </Link>
         </li>
         <li>
-          <Link to="/charts" onClick={toggleMenu} className="block p-4 pl-10">
-            Charts
+          <Link to="/charts" onhover={toggleDropDown} className="block p-4 pl-10">
+          <div>
+            <button
+              onClick={toggleDropDown}  
+            >
+              Charts
+            </button>
+            <li className="list-none">
+              {isDropDown && toggleMenu &&  (
+                <ul className="pl-10 bg-gray-700">
+                  <li>
+                    <Link
+                      to="Line Chart"
+                      className="block p-4 hover:bg-gray-600"
+                      onClick={toggleMenu}
+                    >
+                      Graph Type 1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="Bar Chart"
+                      className="block p-4 hover:bg-gray-600"
+                      onClick={toggleMenu}
+                    >
+                      Graph Type 2
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="Pie Chart"
+                      className="block p-4 hover:bg-gray-600"
+                      onClick={toggleMenu}
+                    >
+                      Graph Type 3
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+          </div>
           </Link>
         </li>
       </ul>
